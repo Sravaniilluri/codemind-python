54c777653b0248d6bdf9f117ab6acb1d9a443acc
Sravaniilluri <98579150+Sravaniilluri@users.noreply.github.com>
2023-06-30 21:55:49 +0530

    s=int(input()) l=list(map(int,input().split())) m=s//2-1 n=s//2 for i in l:     a=l[:m:-1]     b=l[:n:]     c=a+b print(*c)

--- Half_List.py
+++ Half_List.py
@@ -1,8 +1,9 @@
-n=int(input())
+s=int(input())
 l=list(map(int,input().split()))
-m=n//2-1
-n=n//2
-c=l[:m:-1]
-b=l[:n:]
-a=c+b
-print(*a)
+m=s//2-1
+n=s//2
+for i in l:
+    a=l[:m:-1]
+    b=l[:n:]
+    c=a+b
+print(*c)
